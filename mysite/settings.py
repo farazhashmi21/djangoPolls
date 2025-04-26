2904399b16a6b583c352fae5539c6463074f3849
farazhashmi21 <farazhashmi9000@yahoo.com>
2025-04-26 18:19:28 +0500

    DB SWITCHED to MySQL Server;

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "pollsapp",
+        "USER": "adminfarazhashmi",
+        "PASSWORD": "gT%$45Dq*&KenU78",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
