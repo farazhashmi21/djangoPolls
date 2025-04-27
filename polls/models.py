b7e895d89febffc1f4b9976f4a4849c1ad5f5308
farazhashmi21 <farazhashmi9000@yahoo.com>
2025-04-27 18:41:14 +0500

    Was Publiched Method Added To The Question Model;

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,7 @@
+import datetime
+from datetime import timezone
 from tkinter.constants import CASCADE
-
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
@@ -8,6 +10,8 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
