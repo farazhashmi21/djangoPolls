49665f69829b8fc5b694d2afbf34115bb066394e
farazhashmi21 <farazhashmi9000@yahoo.com>
2025-04-28 21:34:14 +0500

    Hello App Installed To The Project;

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'hello.apps.HelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
