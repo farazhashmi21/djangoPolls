b0b93be4b8d50131a2666838662752aeffeedf06
farazhashmi21 <farazhashmi9000@yahoo.com>
2025-04-28 20:24:04 +0500

    LOGIC working;

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,13 @@
 from django.http import HttpResponse
-# from django.template import loader
-# from .models import Question
-
+from django.template import loader
+from polls.models import Question
 
 # Create your views here.
 def index(request):
-    # question_list = Question.objects.order_by("pub_date")[:5]
-    # context = {"latest_question_list": question_list}
-    # template = loader.get_template("polls/index.html")
-    # return HttpResponse(template.render(context, request))
-    # return HttpResponse(output)
-    # return HttpResponse("Assalam-O-Alaikum World, From Polls App.")
+    latest_question_list = Question.objects.order_by("pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    template = loader.get_template("polls/index.html")
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You are looking at Question: %s." % question_id)
