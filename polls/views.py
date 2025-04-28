780e5447e5b341dbf91bd8493b88363aa5326525
farazhashmi21 <farazhashmi9000@yahoo.com>
2025-04-28 18:26:51 +0500

    Question displayed on the HttpResponse;

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, Http404
+from polls.models import Question
 # Create your views here.
 def index(request):
     return HttpResponse("Assalam-O-Alaikum World, From Polls App.")
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at Question: %s." % question_id)
+    question_list = Question.objects.order_by("pub_data")[:5]
+    output = ", ".join(q.question_text for q in question_list)
+    return HttpResponse(output)
+    # return HttpResponse("You are looking at Question: %s." % question_id)
 
 def result(request, question_id):
     response = "You are looking at the result of Question: %s"
