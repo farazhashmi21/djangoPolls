df2ce109d04e87dbb7ae50241bfb14e33b4ffb42
farazhashmi21 <farazhashmi9000@yahoo.com>
2025-04-28 20:30:54 +0500

    Render() function tried;

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponse
-from django.template import loader
+# from django.template import loader
 from polls.models import Question
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by("pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
-    template = loader.get_template("polls/index.html")
-    return HttpResponse(template.render(context, request))
+    # template = loader.get_template("polls/index.html")
+    return render(request, "polls/index.html", context)
+    # return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You are looking at Question: %s." % question_id)
