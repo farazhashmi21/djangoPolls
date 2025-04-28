6057f2b5ed59334ee1144b64fb2528f7e2f8ee4a
farazhashmi21 <farazhashmi9000@yahoo.com>
2025-04-28 18:33:28 +0500

    Funcion SWITCHED with LOGIC;

--- polls/views.py
+++ polls/views.py
@@ -2,13 +2,13 @@ from django.http import HttpResponse, Http404
 from polls.models import Question
 # Create your views here.
 def index(request):
-    return HttpResponse("Assalam-O-Alaikum World, From Polls App.")
-
-def detail(request, question_id):
     question_list = Question.objects.order_by("pub_date")[:5]
     output = ", ".join(q.question_text for q in question_list)
     return HttpResponse(output)
-    # return HttpResponse("You are looking at Question: %s." % question_id)
+    # return HttpResponse("Assalam-O-Alaikum World, From Polls App.")
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at Question: %s." % question_id)
 
 def result(request, question_id):
     response = "You are looking at the result of Question: %s"
