4bc19d0200af971f55912a5d2a71aa9f668c3b62
farazhashmi21 <farazhashmi9000@yahoo.com>
2025-04-28 17:52:11 +0500

    NEW VIEWS MAPPED IN URLSCONFS;

--- polls/urls.py
+++ polls/urls.py
@@ -19,4 +19,7 @@ from polls import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/vote/", views.vote, name = "vote"),
+    path("<int:question_id>/results", views.result, name="results"),
 ]
