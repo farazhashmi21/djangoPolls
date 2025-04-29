8fd02787376ec94ea741acc04ec81c74968b6ffc
farazhashmi21 <farazhashmi9000@yahoo.com>
2025-04-29 19:24:27 +0500

    Views Upgraded To Generic VIEWS;

--- polls/views.py
+++ polls/views.py
@@ -4,27 +4,23 @@ from django.shortcuts import render, get_object_or_404
 from django.db.models import F
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+    def get_queryset(self):
+        return Question.objects.order_by("pub_date")[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/details.html"
+
+class ResultVIew(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by("pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    # template = loader.get_template("polls/index.html")
-    return render(request, "polls/index.html", context)
-    # return HttpResponse(template.render(context, request))
-
-def detail(request, question_id):
-    # try:
-    question = get_object_or_404(Question, pk=question_id)
-        # quesiion = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question Does Not Exist!")
-    return render(request, "polls/detail.html", {"question": question})
-    # return HttpResponse("You are looking at Question: %s." % question_id)
-
-def result(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
