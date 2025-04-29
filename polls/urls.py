3c4a42f2ff36fa887d277be80a3a206ab9d8e967
farazhashmi21 <farazhashmi9000@yahoo.com>
2025-04-29 19:05:14 +0500

    URLs converted to Generic URLs;

--- polls/urls.py
+++ polls/urls.py
@@ -19,8 +19,8 @@ from polls import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='home'),
-    path("<int:question_id>/", views.detail, name="detail"),
+    path('', views.IndexView.as_view(), name='home'),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:question_id>/vote/", views.vote, name = "vote"),
-    path("<int:question_id>/results", views.result, name="results"),
+    path("<int:pk>/results", views.ResultView.as_view(), name="results"),
 ]
