c5ff6398a5dbd1cd68afe6decb10490a0abc87d2
farazhashmi21 <farazhashmi9000@yahoo.com>
2025-05-01 18:51:36 +0500

    Static Routes Defined;

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 from django.conf.global_settings import STATICFILES_DIRS
 
@@ -130,7 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+  os.path.join(BASE_DIR, "/polls/static/")
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
