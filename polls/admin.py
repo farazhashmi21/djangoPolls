fe31cced3920bd0f9f9b7cbec6276e860309a167
farazhashmi21 <farazhashmi9000@yahoo.com>
2025-05-02 17:57:32 +0500

    EXTRA code removed;

--- polls/admin.py
+++ polls/admin.py
@@ -3,9 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from polls.models import Question, Choice
 
-# admin.site.register(Choice)
-# admin.site.register(Question)
-
 class ChoiceInLine(admin.StackedInline):
     model = Choice
     extra = 3
@@ -15,7 +12,6 @@ class QuestionAdmin(admin.ModelAdmin):
         (None, {"fields": ["question_text"]}),
         ("Date Information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
-    inlines = ChoiceInLine
+    inlines = [ChoiceInLine]
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
