293b400dda1d1713aaeb0e0cd50b73dc94af6b90
farazhashmi21 <farazhashmi9000@yahoo.com>
2025-05-02 17:55:02 +0500

    Choice Model Added and Modified into the Admin site;

--- polls/admin.py
+++ polls/admin.py
@@ -6,9 +6,16 @@ from polls.models import Question, Choice
 # admin.site.register(Choice)
 # admin.site.register(Question)
 
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date Information", {"fields": ["pub_date"]}),
+        ("Date Information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    inlines = ChoiceInLine
+
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
